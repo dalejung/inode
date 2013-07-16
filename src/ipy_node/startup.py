5b1d657288ffc396f57a20388494bcb657dded3d
Dale Jung <dale@dalejung.com>
2013-07-16 14:28:48 -0400

    playing around with different DataFrame to_json

--- src/ipy_node/startup.py
+++ src/ipy_node/startup.py
@@ -1,6 +1,15 @@
 import pandas as pd
+from pandas import json
 from IPython.display import JSON
+
+def dataframe_json(df):
+    data = {}
+    for k, v in df.iteritems():
+        data[k] = v.values
+    data['index'] = df.index
+    data['__repr__'] = repr(df)
+    return json.dumps(data)
 
 def to_json(obj):
     if isinstance(obj, pd.DataFrame):
-        return JSON(obj.to_json())
+        return JSON(dataframe_json(obj))
