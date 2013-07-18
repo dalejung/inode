f9b10f56bf73feab4661c0ddf7f506eb60ae3394
Dale Jung <dale@dalejung.com>
2013-07-17 23:36:39 -0400

    changed ipython to only return ipython js module

--- src/ipy_node/ipython.js
+++ src/ipy_node/ipython.js
@@ -9,8 +9,9 @@
  * Exports
  *  IPython
  */ 
-var fs = require('fs');
-var Contextify = require('contextify');
+var fs = require('fs'),
+  Contextify = require('contextify'),
+  path = require("path");
 
 /*
  * IPython requires the following externalities:
@@ -18,25 +19,29 @@ var Contextify = require('contextify');
  *  `document.cookies`
  *  WebSocket
  */
-var $ = require('jquery').create();
-// kernel needs document var for document.cookies
-window = $('html').parent().get(0).parentWindow
-document = window.document
-var WebSocket = require('ws');
-var sandbox = { 
-  console : console, 
-  $ : $, 
-  WebSocket : WebSocket, 
-  setTimeout : setTimeout,
-  document : document,
-};
+function create_ipython() {
+  var $ = require('jquery').create();
+  // kernel needs document var for document.cookies
+  window = $('html').parent().get(0).parentWindow
+  document = window.document
+  var WebSocket = require('ws');
+  var sandbox = { 
+    console : console, 
+    $ : $, 
+    WebSocket : WebSocket, 
+    setTimeout : setTimeout,
+    document : document,
+  };
 
-Contextify(sandbox);
-// run the combined src scripts
-var filename = require.resolve('./ipy_node.js');
-var content = fs.readFileSync(filename, 'utf8');
-sandbox.run(content);
+  Contextify(sandbox);
 
-module.exports.IPython = sandbox.IPython
-module.exports.IPythonBridge = sandbox.IPythonBridge
-module.exports.sandbox = sandbox;
+  IPYTHON_SCRIPTS = ['namespace.js', 'utils.js', 'kernel.js'];
+  for (var i=0; i < IPYTHON_SCRIPTS.length; i++) {
+    var script = IPYTHON_SCRIPTS[i];
+    var content = fs.readFileSync(path.join(__dirname, 'src/ipycli', script), 'utf8');
+    sandbox.run(content);
+  }
+  return sandbox.IPython
+}
+
+module.exports = create_ipython();
