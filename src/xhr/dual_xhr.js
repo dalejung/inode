ca79211215ce732c24f2abc780cd8b27b67a95ec
Dale Jung <dale@dalejung.com>
2013-07-19 18:05:33 -0400

    moved src->lib

--- src/xhr/dual_xhr.js
+++
@@ -1,6 +0,0 @@
-var xmlhttp  = require("xmlhttprequest");   
-var xhr_local = require('./xhr_local.js')
-
-var XMLHttpRequest = xhr_local.XMLHttpRequest
-
-module.exports.XMLHttpRequest = XMLHttpRequest
