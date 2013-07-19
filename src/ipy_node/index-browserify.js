92b20db25588d599ab14b13c699bd69691a56868
Dale Jung <dale@dalejung.com>
2013-07-19 17:09:31 -0400

    moved ipy_node to own repo

--- src/ipy_node/index-browserify.js
+++
@@ -1,7 +0,0 @@
-var IPython = require('./lib/ipython.js');
-var Bridge = require('./lib/bridge.js');
-
-ipy_node = {
-  Bridge : Bridge, 
-  IPython : IPython
-}
--- src/ipy_node/index.js
+++
@@ -1,4 +0,0 @@
-var IPython = require('./lib/ipython.js');
-var Bridge = require('./lib/bridge.js');
-module.exports.IPython = IPython;
-module.exports.Bridge = Bridge;
